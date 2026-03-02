837fa2196ae96c8e75ed94be068ca29e63ff1410
abdulrahmanaymandev <abdulrahman.ayman.dev@gmail.com>
2026-03-03 02:41:23 +0300

    feat: support FRONTEND_URL env variable in CORS config

--- config/cors.php
+++ config/cors.php
@@ -1,27 +1,16 @@
 <?php
 
 return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
-    */
-
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
-
     'allowed_methods' => ['*'],
-
-    'allowed_origins' => ['http://localhost:5173', 'http://127.0.0.1:5173'],
-
+    'allowed_origins' => array_filter([
+        'http://localhost:5173',
+        'http://127.0.0.1:5173',
+        env('FRONTEND_URL'),
+    ]),
     'allowed_origins_patterns' => [],
-
     'allowed_headers' => ['*'],
-
     'exposed_headers' => [],
-
     'max_age' => 0,
-
     'supports_credentials' => false,
-
 ];
